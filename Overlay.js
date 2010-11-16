f65148e97b8072188c25c4a1e6b522ba302d422f
Owen Williams <owen@dynabooks.com>
2010-11-16 14:01:14 -0500

    added $loadingMessage global concept only show $close icon when autoHide is true

--- Overlay.js
+++ Overlay.js
@@ -9,14 +9,19 @@ new $Section.Subclass("$Overlay", {
 		visible : false,
 		template : "<mask part='overlay:$mask'>\
 						<border part='overlay:$border'>\
-							<close part='overlay:$close'></close>\
+							<close part='overlay:$close' visible='no'></close>\
 							<container></container>\
 						</border>\
 					</mask>",
 		autoHide : new Attribute({name:"autoHide", type:"flag", inherit:true, value:true}),
 		listeners : "click:onClick",
 		
+		onReady : function() {
+			this.$close.visible = this.autoHide;
+		},
+		
 		onClick : function(event) {
+			if (!this.autoHide) return;
 			if (event.target === this.$close ||
 				 (event.target == this.$mask && this.autoHide)) this.visible = false;
 		}
@@ -24,6 +29,24 @@ new $Section.Subclass("$Overlay", {
 });
 
 
+// create an overlay w/id "loadingMessage" to show/hide messages when loading
+new $Overlay.Subclass("$LoadingMessage", {
+	tag : "overlay",
+	selector : "#loadingMessage",
+	properties : {
+		template : "<mask part='overlay:$mask'>\
+						<border part='overlay:$border'>\
+							<close part='overlay:$close' visible='no'></close>\
+							<container><notice part='overlay:$notice'></notice></container>\
+						</border>\
+					</mask>",
+
+		showMessage : function(message) {
+			this.$notice.html = message;
+			this.visible = true;
+		}
+	}
+});
 
 Script.loaded("{{hope}}Overlay.js");
 });
